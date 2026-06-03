98369e9141bb2151b7222929f2b8eef150ee0bd5
Hammad-103 <hamadaly.x@gmail.com>
2026-06-03 22:43:02 +0500

    fix double messages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <App />
-  </StrictMode>,
+  
 )
 
 import App from './App'
